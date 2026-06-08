9f8a85ad27389bed37cc16d04366126b75744868
Bhoomika Agarwal <bbhoomikaagrawal@gmail.com>
2026-06-09 00:15:54 +0530

    Add rock paper scissors game

---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,20 @@
+import random
+player = input("Enter rock, paper, or scissors: ").lower()
+options=["rock","paper","scissors"]
+if player not in options:
+    print("Invalid choice! Please enter rock, paper, or scissors.")
+else:
+   options=["rock","paper","scissors"]
+   computer=random.choice(options)
+   print("you chose:",player)
+   print("computers choice:",computer)
+   if player==computer:
+      print("its a tie")
+   elif (
+      (player=="rock" and computer=="scissors")or
+      (player=="scissors" and computer=="paper")or
+      (player=="paper" and computer=="rock")
+   ):
+      print("you win")
+   else:
+      print("computer wins")
