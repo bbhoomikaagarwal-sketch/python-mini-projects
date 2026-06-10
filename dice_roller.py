d9c51e4b7cff3579275e11d5efc11ddea228cd28
Bhoomika Agarwal <bbhoomikaagrawal@gmail.com>
2026-06-11 01:04:16 +0530

    save changes

--- dice_roller.py
+++
@@ -1,9 +0,0 @@
-import random
-player=input("do you want to roll the dice (yes or no):")
-while player=="yes":
-    dice=random.randint(1,6)
-    print("the number on dice is:",dice)
-
-    player=input("roll again(yes or no):").lower()
-
-print("exit")
