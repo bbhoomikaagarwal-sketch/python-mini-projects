434193cec084d00bca8f6771d4ae64b450bc498c
Bhoomika Agarwal <bbhoomikaagrawal@gmail.com>
2026-06-13 15:00:21 +0530

    add changes

--- projects/bank management system.py
+++ projects/bank management system.py
@@ -1,7 +1,7 @@
       #welcome to peoples bank
 print("welcome to people's bank")
 balance=0
-transactions=0
+transactions=[]
 while True:
     print("enter 1 to check the balance")
     print("enter 2 to deposit the money")
@@ -11,25 +11,30 @@ while True:
     choice=int(input("enter the number for your service:"))
     if choice==1:
       print("your balance is:",balance)
+      
     elif choice==2:
       amount=int(input("the amount is:",))
       balance+=amount
-      transactions+=1
+      transactions.append(f"deposited {amount}")
       print("deposited successfully")
       print("current balance is:",balance)
     elif choice==3:
       amount=int(input("enter the amount to withdraw:"))
       if amount<=balance:
        balance-=amount
-       transactions+=1
+       transactions.append(f"withdrawn {amount}")
        print(amount,"withdrawn successfully")
-       print("Current balance is:", balance)
+       print("Current balance is:", balance)  
       else :
        print("insufficient funds")
     elif choice==4:
       break
     elif choice==5:
-      print("total transactions:",transactions)
+      if len(transactions)==0:
+        print("no transactions yet")
+      else:
+        print("total transactions:",transactions)
+        print("remaining balance:",balance)
     else:
       print("invalid option")
 
